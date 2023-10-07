0ca87be18064fa1b8af62847a7b4b7e32574596e
Emily Travinsky <eberkson@email.arizona.edu>
2023-10-07 16:13:58 -0700

    Steps 1 - 3 (EDA through Data Testing)

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -3,7 +3,13 @@ import numpy as np
 import scipy.stats
 
 
-def test_column_names(data):
+def test_column_names(data: pd.DataFrame):
+    """
+    Function to test that expected columns are present in the dataframe.
+
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
+    """
 
     expected_colums = [
         "id",
@@ -30,7 +36,13 @@ def test_column_names(data):
     assert list(expected_colums) == list(these_columns)
 
 
-def test_neighborhood_names(data):
+def test_neighborhood_names(data: pd.DataFrame):
+    """
+    Function to test that neighborhood_names are valid.
+
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
+    """
 
     known_names = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"]
 
@@ -42,8 +54,12 @@ def test_neighborhood_names(data):
 
 def test_proper_boundaries(data: pd.DataFrame):
     """
-    Test proper longitude and latitude boundaries for properties in and around NYC
+    Test proper longitude and latitude boundaries for properties in/around NYC.
+
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
     """
+
     idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
 
     assert np.sum(~idx) == 0
@@ -51,15 +67,41 @@ def test_proper_boundaries(data: pd.DataFrame):
 
 def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_threshold: float):
     """
-    Apply a threshold on the KL divergence to detect if the distribution of the new data is
-    significantly different than that of the reference dataset
+    Compare distribution of input dataset to reference dataset.
+
+    Apply a threshold on the KL divergence to detect if the distribution of the
+    new data is significantly different than that of the reference dataset.
+
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
+        ref_data (pd.DataFrame): Reference dataframe for rental pricing.
+        kl_threshold (float): Threshold to check for dataset divergence.
     """
+
     dist1 = data['neighbourhood_group'].value_counts().sort_index()
     dist2 = ref_data['neighbourhood_group'].value_counts().sort_index()
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data: pd.DataFrame):
+    """
+    Function to test that input dataframe has a reasonable number of rows.
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
+    """
+
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Function to check that the nightly price falls within a certain range.
+
+    Args:
+        data (pd.DataFrame): Input dataframe for rental pricing.
+        min_price (float): Lower bound for nightly pricing (in USD).
+        max_price (float): Upper bound for nightly pricing (in USD).
+    """
+    
+    assert data['price'].between(min_price, max_price).all()
+
